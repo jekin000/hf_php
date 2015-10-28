34700145b653161c768d63457b7b9749d28dd28e
jekin000 <kira_r163@163.com>
2015-10-28 17:01:45 +0800

    hf_ch06 [12] README 2.5 web framework

--- ch06/approvescore.php
+++ ch06/approvescore.php
@@ -1,4 +1,21 @@
 <?php
-    echo 'Hello World<br>';
+    require_once('authorize.php');
 ?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Guitar Wars - Appove a Score</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+    <h2>Guitar Wars - Appove a Score</h2>
+
+
+<?php
+    echo '<p><a href=admin.php>&lt;&lt; Back to admin page</a></p>'
+?>
+</body>
+</html>
+
 
