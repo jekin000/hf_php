634f01d4ceae37896c3f7b51560571c9cc177888
jekin000 <kira_r163@163.com>
2015-10-28 11:08:49 +0800

    hf_ch06 [1] init chapter 06

---
+++ ch06/admin.php
@@ -0,0 +1,25 @@
+<?php
+    require_once('appvars.php');
+    require_once('connectvars.php');
+
+    $dbc = mysqli_connect(DB_HST,DB_USR,DB_PWD,DB_NAM)
+        or die("Can not connect to database.");
+    $query = "SELECT * FROM guitarwars ORDER BY score DESC, date ASC";
+    $data = mysqli_query($dbc, $query)
+        or die("Query failed.");
+    
+    echo '<table>';
+    while ($row=mysqli_fetch_array($data)){
+        echo '<tr class="scorerow"><td><strong>'.$row['name'].'</strong></td>';
+        echo '<td>'.$row['date'].'</td>';
+        echo '<td><a href="removescore.php?id='.
+                $row['id'].
+                '&amp;date='.$row['date'].
+                '&amp;name='.$row['name'].
+                '&amp;score='.$row['score'].
+                '&amp;screenshot='.$row['screenshot'].
+                '">Remove</a></td></tr>';
+    }
+    echo '</table>';    
+    mysqli_close($dbc);
+?>
