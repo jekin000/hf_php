7a8b842c431ac6c5e7d523f6968ba21df1c36785
jekin000 <kira_r163@163.com>
2016-01-05 15:04:54 +0800

    hf_ch08 [4] update questionnaire framwork,no data

--- ch08/questionnaire.php
+++ ch08/questionnaire.php
@@ -1,19 +1,36 @@
 <?php
-  require_once('appvars.php');
-  require_once('connectvars.php');
+    require_once('appvars.php');
+    require_once('connectvars.php');
 ?>
 
 <?php
-  session_start();
+    session_start();
 ?>
 
 <?php
-  $page_title = 'Questionnaire';
-  require_once('header.php');
-  require_once('navmenu.php');
+    $page_title = 'Questionnaire';
+    require_once('header.php');
+
+    if (!isset($_SESSION['user_id'])){
+        echo '<p class="login">Please <a href="login.php">Log In</a> to access this page.</p>';
+        exit();
+    }
+    require_once('navmenu.php');
 ?>
 
 <?php
-  require_once('footer.php');
+    echo '<form method="post" action="'.$_SERVER['PHP_SELF'].'">';
+    echo '<p>How do you feel about each topic?</p>';
+    echo '<fieldset><legend>Hobby</legend>';
+    echo '<label for="2">Movie</label>';
+    echo '<input type="radio" id="2" name="2" value="1"/>Love ';
+    echo '<input type="radio" id="2" name="2" value="2"/>Hate ';
+    echo '</fieldset>';
+    
+    echo '<input type="submit" value="Save Questionnaire" name="submit">';
+    echo '</form>';
+?>
+<?php
+    require_once('footer.php');
 ?>
 
