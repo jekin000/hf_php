ee23309eeaf003eca411b4a0f11b534824f774f8
jekin000 <kira_r163@163.com>
2016-01-05 11:27:01 +0800

    hf_ch08 [3] add questionnaire to menu.

---
+++ ch08/questionnaire.php
@@ -0,0 +1,19 @@
+<?php
+  require_once('appvars.php');
+  require_once('connectvars.php');
+?>
+
+<?php
+  session_start();
+?>
+
+<?php
+  $page_title = 'Questionnaire';
+  require_once('header.php');
+  require_once('navmenu.php');
+?>
+
+<?php
+  require_once('footer.php');
+?>
+
