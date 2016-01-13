8bb47c52e277615db6a4d809e60dae827f464b47
jekin000 <kira_r163@163.com>
2016-01-13 11:17:30 +0800

    hf_ch08 [11] finish user mismatch.w

--- ch08/mymismatch.php
+++ ch08/mymismatch.php
@@ -61,6 +61,7 @@
 ?>
 
 <?php
+    /* search for all other mismatcher.*/
     foreach ($user_ids as $id) {
         $score = 0;
         $topics = array();
@@ -86,29 +87,60 @@
         for ($i=0; $i<count($user_response); $i++){
             if ($user_response[$i]['response']+$response[$i]['response']==3){
                 $score++;
-                array_push($topics,$response['name']);
+                array_push($topics,$response[$i]['name']);
             }
         }
 
         if ($score > $mismatch_score){
             $mismatch_score = $score;
-            $mismatch_topic = array_slice($topics);
+            $mismatch_topic = array_slice($topics,0);
             $mismatch_user_id = $id;
         }
     }
+?>
 
-    if ($mismatch_user_id > -1){
-        echo 'Your mismatcher is '.$mismatch_user_id;
+<?php
+    /* print result.*/ 
+    if ($mismatch_user_id == -1){
+        echo 'Sorry, no mismatcher.<br /> ';
+        echo 'If you do not finish the <a href="questionnaire.php">Questionnaire</a>,we suggest you finish it first.';
     }
     else{
-        echo 'Sorry ,no mismatcher';
+        $query = "SELECT username,first_name,last_name,city,state,picture FROM mismatch_user"
+                ." WHERE user_id=".$mismatch_user_id;
+        $data = mysqli_query($dbc,$query)
+            or die($query);
+        if (mysqli_num_rows($data) !=1){
+            echo 'Find wrong mismatcher, query data='.mysqli_num_rows($data);
+        }
+        else{
+            $row = mysqli_fetch_array($data);
+            echo '<table><tr><td class="label">';
+            if (!empty($row['first_name']) && !empty($row['last_name'])){
+                echo $row['first_name'].' '.$row['last_name'].'<br />';
+            }
+            if (!empty($row['city']) && !empty($row['state'])){
+                echo $row['city'].' '.$row['state'].'<br />';
+            }
+
+            echo '</td><td>';
+            if (!empty($row['picture'])){
+                echo '<img src="'.MM_UPLOADPATH.$row['picture'].'" alt="Profile Picture" /><br />';
+            }
+            echo '</td><tr></table>';
+
+            echo '<h4>You are mismatched on the following '.count($mismatch_topic).' topics:</h4>';
+            foreach ($mismatch_topic as $tpc){
+                echo $tpc.'<br />';
+            }
+            echo '<h4>View <a href=viewprofile.php?user_id='.$mismatch_user_id.'>'.$row['first_name'].'\'s Profile</a>.</h4>';
+        }
     }
 ?>
 
 <?php
     mysqli_close($dbc);
 ?>
-
 
 <?php
     require_once('footer.php');
